6d9056211087c3b7da3c81c27f7d53ad3bbfdd01
lovaire <rifsrahmt@gmail.com>
2026-02-16 23:02:38 +0700

    tes deployment pertama

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <h1>Users List</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.username }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      users: []  // Menyimpan data pengguna yang diterima dari backend
+    };
+  },
+  mounted() {
+    // Lakukan request GET ke backend
+    axios.get('http://localhost:8080/api/users')  // URL backend kamu
+      .then(response => {
+        this.users = response.data;  // Menyimpan data pengguna
+      })
+      .catch(error => {
+        console.log('Error fetching data:', error);
+      });
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
