289d2cee0542a14a82bf87b82e72ee9a1ac6ef3d
Rifqi Rahmatuloh <rifqi.rahmatuloh@ui.ac.id>
2026-04-19 17:13:36 +0700

    Edit axios.js (delete import)

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,7 +4,7 @@ import { authService } from '@/services/auth.service'
 
 const api = axios.create({
   // ✅ Pakai import.meta.env untuk Vite + prefix VITE_
-  baseURL: import.process.env.VUE_APP_API_URL || '/api',
+  baseURL: process.env.VUE_APP_API_URL || '/api',
   timeout: 20000,
   headers: {
     'Content-Type': 'application/json'
